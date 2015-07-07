129851a52ce56e83b16e98173116d38b074690e9
author2006 <author2006@ukr.net>
2015-07-08 00:40:36 +0300

    Add static files, main template and some views functions.

--- ftp_client/views.py
+++ ftp_client/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from ftp_client.forms import ConnectionForm
 
-# Create your views here.
+
+def index(request):
+    connection_form = ConnectionForm()
+    return render(request, template_name='ftp_client/index.html', context={
+        'connection_form': connection_form,
+    })
+
+def connect(request):
+    connection_form = ConnectionForm(request.POST)
+
+    if connection_form.is_valid():
+        return HttpResponse('VALID')
+    return HttpResponse(connection_form.errors.as_json())
